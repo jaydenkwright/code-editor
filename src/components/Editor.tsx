b4418054f4518e683ac0bb02bf0853f7a2ec1948
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-15 19:34:19 -0400

    Created basic code editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
 
-const Editor = () => {
+const Editor: React.FC<any> = ({ displayName, language, value, onChange }) => {
+    const handleChange = (editor: any, data: any, value: any) => {
+        onChange(value)
+    }
     return (
-        <div>
-            
+        <div className='editor-container'>
+            <div className='editor-title'>
+                {displayName}
+            </div>
+            <ControlledEditor 
+                onBeforeChange={handleChange}
+                value={value}
+                className='editor-wrapper'
+                options = {{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'material'
+                }}
+            />
         </div>
     )
 }
