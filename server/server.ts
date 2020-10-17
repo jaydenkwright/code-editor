15d8cf334e30d1dfe2d5a830b1b9a7609377bb32
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-17 18:18:24 -0400

    Added express.json() and cors middleware

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,17 @@
 import express from 'express'
+import code from './routes/code'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app: express.Application = express()
+app.use(express.json())
 
-app.listen(5000, () => {
-    console.log('started')
+app.use('/api/code', code)
+app.use('/api/project/', () => {})
+app.use('/api/user/', () => {})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
 })
