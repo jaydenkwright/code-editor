da619a6a248ff3682adc0d751ee37e06a19453ee
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-17 18:16:49 -0400

    Created postgres database connection

---
+++ server/db/db.ts
@@ -0,0 +1,8 @@
+import { Pool } from 'pg'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+export const pool = new Pool({
+    connectionString: connectionString,
+    ssl: false,
+})
