9685092d4727261c727cd887c1f11991d1bcf15f
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-18 16:23:12 -0400

    Added cookieParser middleware

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,20 @@
 import express from 'express'
 import code from './routes/code'
+import projects from './routes/projects'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import user from './routes/user'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 
 const app: express.Application = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 
 app.use('/api/code', code)
-app.use('/api/project/', () => {})
-app.use('/api/user/', () => {})
+app.use('/api/project/', projects)
+app.use('/api/user/', user)
 
 const port = process.env.PORT || 5000
 
