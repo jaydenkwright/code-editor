c25fbb6ef2a810502c40c4f115111432e22457b0
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-18 16:22:37 -0400

    Created login route

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Router } from 'express'
 import { pool } from '../db/db'
 import Joi from '@hapi/joi'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 const router = Router()
 
 const registrationSchema = Joi.object({
@@ -12,44 +13,68 @@ const registrationSchema = Joi.object({
 })
 
 router.post('/register', async (req: Request, res: Response) => {
-    const { error } = registrationSchema.validate(req.body)
-    if (error) res.status(400).json({"msg": "Something went wrong!"})
-    const { name, username, email, password } = req.body
-    const emailExists = await pool.query(
-        "SELECT * FROM users WHERE email = $1",
-        [email]
-    )
-    if (emailExists.rows[0]) return res.status(400).json({"msg": "Email is already in use!"})
+    try{
+        const { error } = registrationSchema.validate(req.body)
+        if (error) return res.status(400).json({"msg": "Something went wrong!"})
+        const { name, username, email, password } = req.body
+        const emailExists = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        )
+        if (emailExists.rows[0]) return res.status(400).json({"msg": "Email is already in use!"})
 
-    const usernameExists = await pool.query(
-        "SELECT * FROM users WHERE username = $1",
-        [username]
-    )
-    if (usernameExists.rows[0]) return res.status(400).json({"msg": "Username is already taken!"})
+        const usernameExists = await pool.query(
+            "SELECT * FROM users WHERE username = $1",
+            [username]
+        )
+        if (usernameExists.rows[0]) return res.status(400).json({"msg": "Username is already taken!"})
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
-    const newUser = await pool.query(
-        "INSERT INTO users (name, username, email, password, date) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP) RETURNING *",
-        [name, username, email, hashedPassword]
-    )
+        const newUser = await pool.query(
+            "INSERT INTO users (name, username, email, password, date) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP) RETURNING *",
+            [name, username, email, hashedPassword]
+        )
 
-    res.json({"msg": "User created"})
+        res.json({"msg": "User created"})
+    }catch(error){
+        res.json({"msg": "Something went wrong!"})
+    }
 })
 
 const loginSchema = Joi.object({
-    name: Joi.string().min(1).max(50).required(),
-    username: Joi.string().min(3).max(100).required(),
     email: Joi.string().min(7).max(200).required().email(),
     password: Joi.string().min(6).max(300).required()
 })
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', async (req: Request, res: Response) => {
     try {
+        const { error } = loginSchema.validate(req.body)
+        if (error) return res.status(400).json({"msg": "Something went wrong!"})
+
+        const { email, password } = req.body
+        const user = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        )
+        if (!user.rows[0]) return res.status(400).json({"msg": "Email is incorrect"})
         
+        const validatePassword = await bcrypt.compare(password, user.rows[0].password)
+        if (!validatePassword) return res.status(400).json({'msg': 'Password is incorrect'})
+
+        const token = jwt.sign({token: user.rows[0].id}, `${process.env.TOKEN_SECRET}`, {
+            expiresIn: '24h'
+        })
+
+        res.cookie('token', token, {
+            httpOnly: true,
+        })
+       res.header('login-token', token).json({'msg': 'Logged In!'})
+
+
     } catch (error) {
-        
+        res.json({"msg": "Something went wrong!"})
     }
 })
 
