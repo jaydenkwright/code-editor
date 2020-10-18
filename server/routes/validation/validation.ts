aee52b11c26c51a197eb343d88472691767101c4
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-18 17:06:45 -0400

    Created file for validation

---
+++ server/routes/validation/validation.ts
@@ -0,0 +1,13 @@
+import Joi from '@hapi/joi'
+
+export const registrationSchema = Joi.object({
+    name: Joi.string().min(1).max(50).required(),
+    username: Joi.string().min(3).max(100).required(),
+    email: Joi.string().min(7).max(200).required().email(),
+    password: Joi.string().min(6).max(300).required()
+})
+
+export const loginSchema = Joi.object({
+    email: Joi.string().min(7).max(200).required().email(),
+    password: Joi.string().min(6).max(300).required()
+})
