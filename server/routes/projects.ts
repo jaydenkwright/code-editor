8faed27e3d00058b14b059f4232f30c671e39e3a
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-18 16:52:34 -0400

    Added verification middleware to potected routes

--- server/routes/projects.ts
+++ server/routes/projects.ts
@@ -1,13 +1,15 @@
 import express, {Request, Response, Router} from 'express'
 import { pool } from '../db/db'
+import { verify } from './middleware/verify'
 const router = Router()
 
-router.post('/create', async (req: Request, res: Response) => {
+router.post('/create', verify, async (req: any, res: Response) => {
     try {
-        const { userId, title, description, privacy } = req.body
+        const { id } = req.user
+        const { title, description, privacy } = req.body
         const createProject = await pool.query(
             "INSERT INTO projects (userId, title, description, private, date) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP) RETURNING *",
-            [userId, title, description, privacy]
+            [id, title, description, privacy]
         )
         res.json(createProject.rows[0])
     } catch (error) {
@@ -32,13 +34,13 @@ router.get('/user/:userId', async (req: Request, res: Response) => {
     }
 })
 
-router.get('/:id', async (req: Request, res: Response) => {
+router.get('/:projectId', verify, async (req: any, res: Response) => {
     try{
-        const userId = 1
-        const { id } = req.params
+        const { id } = req.user
+        const { projectId } = req.params
         const project = await pool.query(
             "SELECT * FROM projects WHERE id = $1 AND (private = $2 OR userId = $3)",
-            [id, false, userId]
+            [projectId, false, id]
         )
         if (project.rows[0]){
             res.json(project.rows[0])
@@ -50,14 +52,14 @@ router.get('/:id', async (req: Request, res: Response) => {
     }
 })
 
-router.put('/update/:id', async (req: Request, res: Response) => {
+router.put('/update/:projectId', verify, async (req: any, res: Response) => {
     try {
-        const { id } = req.params
-        const userId = 1
+        const { id } = req.user
+        const { projectId } = req.params
         const { title, description, privacy } = req.body
         const updateProject = pool.query(
             "UPDATE projects SET title = $1, description = $2, private = $3 WHERE id = $4 AND userId = $5",
-            [title, description, privacy, id, userId]
+            [title, description, privacy, projectId, id]
         )
         res.json({"msg": "Updated"})
     } catch (error) {
@@ -65,13 +67,13 @@ router.put('/update/:id', async (req: Request, res: Response) => {
     }
 })
 
-router.delete('/delete/:id', async (req: Request, res: Response) => {
+router.delete('/delete/:projectId', verify, async (req: any, res: Response) => {
     try {
-        const userId = 1
-        const { id } = req.params
-        const deleteProject = pool.query(
+        const { id } = req.user
+        const { projectId } = req.params
+        const deleteProject = await pool.query(
             "DELETE FROM projects WHERE id = $1 AND userId = $2",
-            [id, userId]
+            [projectId, id]
         )
         res.json({"msg": "Deleted"})
     } catch (error) {
