a6197fffc7e32a8e89938a03646dd14ec6e3159b
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-17 23:44:51 -0400

    Removed database code from server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import code from './routes/code'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 const app: express.Application = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/code', code)
 app.use('/api/project/', () => {})
