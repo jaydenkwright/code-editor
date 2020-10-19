44ff1e076d5bcc37c12b6a7ad22241faab2fe6d8
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-19 19:25:37 -0400

    Created current state which determines which code editor is currently being displayed

--- client/src/components/EditorPage.tsx
+++ client/src/components/EditorPage.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect} from 'react'
 import Editor from './Editor'
 import useLocalStorage from '../hooks/useLocalStorage'
+import { stringify } from 'querystring'
 
 const EditorPage = () => {
 const [html, setHTML] = useLocalStorage('html', `<h1>Hello World</h1>`)
 const [css, setCSS] = useState<string>(``)
 const [javascript, setJavascript] = useState<string>('')
 const [srcDoc, setSrcDoc] = useState<string>('')
+const [current, setCurrent] = useState('html')
 
 useEffect(() => {
     const timeout = setTimeout(() => {
@@ -22,20 +24,55 @@ useEffect(() => {
     return () => clearTimeout(timeout)
 }, [html, css, javascript])
 
+    interface language {
+        language: String,
+        displayName: String,
+        value: String,
+        onChange: any
+    }
+
+    type languages =  {
+        [key: string]: language
+    }
+
+    const langs: languages = {
+        html: {
+            language: 'xml',
+            displayName: 'HTML',
+            value: html,
+            onChange: setHTML
+        },
+        css: {
+            language: 'css',
+            displayName: 'CSS',
+            value: css,
+            onChange: setCSS,
+        },
+        javascript: {
+            language: 'javascript',
+            displayName: 'Javascript',
+            value: javascript,
+            onChange: setJavascript
+        }
+    }
+
 return (
     <>
         <div className='window-container'>
             <div className='window code-window'>
-                <h3>Kyle</h3>
+                <button onClick={() => setCurrent('html')}>HTML</button>
+                <button onClick={() => setCurrent('css')}>CSS</button>
+                <button onClick={() => setCurrent('javascript')}>Javascript</button>
+                {/* <h3>Kyle</h3>
                 <h3>Portfolio Website</h3>
-                <h4>Designs for Portfolio website.</h4>
+                <h4>Designs for Portfolio website.</h4> */}
                 <Editor 
-                language='xml' 
-                displayName='HTML' 
-                value={html} 
-                onChange={setHTML} 
+                language={langs[current].language}
+                displayName={langs[current].displayName}
+                value={langs[current].value}
+                onChange={langs[current].onChange}
                 />
-                <Editor
+                {/* <Editor
                 language='css' 
                 displayName='CSS' 
                 value={css} 
@@ -46,7 +83,7 @@ return (
                 displayName='Javascript' 
                 value={javascript} 
                 onChange={setJavascript} 
-                />
+                /> */}
             </div>
             <div className='window display'>
                 <iframe 
