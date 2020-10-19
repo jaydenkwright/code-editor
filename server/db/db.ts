34205410e5d5ed1165d644ba53c05e7a40f53c23
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-19 18:11:35 -0400

    Fixed database connection

--- server/db/db.ts
+++ server/db/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const pool = new Pool({
+export const pool: Pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
