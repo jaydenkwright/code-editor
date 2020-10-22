492e6cae64001a1bca799489c2db186d185175bb
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-21 20:19:59 -0400

    Created page to display login component

---
+++ client/src/components/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Login'
+
+const LoginPage = () => {
+    return (
+        <div className='container'>
+            <div className='loginRegistrationContainer'>
+                <Login />
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
