ea82f20e950e6543f7ec73b1ab07d0db29c42163
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-21 20:21:45 -0400

    Created isLoggedIn function to determine if current user is logged in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import api from './api/api'
 import EditorPage from './components/EditorPage'
+import GettingStarted from './components/GettingStarted'
+import LoginPage from './components/LoginPage'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState<boolean | null>(null)
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      try {
+        const response = await api.get('/user/isLoggedIn',
+        {
+          withCredentials: true
+        })
+        setLoggedIn(true)
+      } catch (error) {
+        setLoggedIn(false)
+      }
+    }
+
+    isLoggedIn()
+  }, [loggedIn])
+
   return (
-    <>
-      <EditorPage />
-    </>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <EditorPage />
+        </Route>
+        <Route path='/getStarted' exact>
+          <GettingStarted />
+        </Route>
+        <Route path='/login' exact>
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
