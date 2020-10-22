22646d211b00288f56dbe2dd84c5bb0040fe019b
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-22 17:07:54 -0400

    Fixed home page link

--- client/src/components/EditorPage.tsx
+++ client/src/components/EditorPage.tsx
@@ -1,95 +1,93 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Editor from './Editor'
 import useLocalStorage from '../hooks/useLocalStorage'
 import { languages } from '../interfaces/EditorInterface'
-import { Button, H1, H3 } from '../styles/styles'
+import { Button, H1, H3, SubmitButton } from '../styles/styles'
+import UserContext from '../context/UserContext'
+import { useHistory } from 'react-router-dom'
 
 const EditorPage = () => {
-const [html, setHTML] = useLocalStorage('html', `<h1>Hello World</h1>`)
-const [css, setCSS] = useLocalStorage('css', '')
-const [javascript, setJavascript] = useLocalStorage('javascript', '')
-const [srcDoc, setSrcDoc] = useState<string>('')
-const [current, setCurrent] = useState('html')
+    const history = useHistory()
+    const user = useContext<any>(UserContext)
+    const { loggedIn } = user
+    const [html, setHTML] = useLocalStorage('html', `<h1>Hello World</h1>`)
+    const [css, setCSS] = useLocalStorage('css', '')
+    const [javascript, setJavascript] = useLocalStorage('javascript', '')
+    const [srcDoc, setSrcDoc] = useState<string>('')
+    const [current, setCurrent] = useState('html')
 
-useEffect(() => {
-    const codeTimeOut = () => {
-        const timeout = setTimeout(() => {
-            setSrcDoc(`
-            <html>
-                <body>${html}</body>
-                <style>${css}</style>
-                <script>${javascript}</script>
-            </html>
-            `)
-         }, 1000)
-        
-        return () => clearTimeout(timeout)
-    }
+    useEffect(() => {
+            const timeout = setTimeout(() => {
+                setSrcDoc(`
+                <html>
+                    <body>${html}</body>
+                    <style>${css}</style>
+                    <script>${javascript}</script>
+                </html>
+                `)
+            }, 1000)
+            
+            return () => clearTimeout(timeout)
 
-    codeTimeOut()
+    }, [html, css, javascript])
 
-}, [html, css, javascript])
-
-    const langs: languages = {
-        html: {
-            language: 'xml',
-            displayName: 'HTML',
-            value: html,
-            onChange: setHTML
-        },
-        css: {
-            language: 'css',
-            displayName: 'CSS',
-            value: css,
-            onChange: setCSS,
-        },
-        javascript: {
-            language: 'javascript',
-            displayName: 'Javascript',
-            value: javascript,
-            onChange: setJavascript
+        const langs: languages = {
+            html: {
+                language: 'xml',
+                displayName: 'HTML',
+                value: html,
+                onChange: setHTML
+            },
+            css: {
+                language: 'css',
+                displayName: 'CSS',
+                value: css,
+                onChange: setCSS,
+            },
+            javascript: {
+                language: 'javascript',
+                displayName: 'Javascript',
+                value: javascript,
+                onChange: setJavascript
+            }
         }
-    }
-
-    const loggedIn = true
-return (
-    <>
-        <div className='window-container'>
-            <div className='window code-window'>
-                {
-                loggedIn ? <>
-                    <H3>Home</H3>
-                    <H1>Untitled</H1>
+    return (
+        <>
+            <div className='window-container'>
+                <div className='window code-window'>
+                    {
+                    loggedIn ? <>
+                        <H3 onClick={() => history.push('/')}>Home</H3>
+                        <H1>Untitled</H1>
+                        </>
+                    : <>
+                        <SubmitButton block={true} onClick={() => history.push('/getStarted')} noWidth={true} >Get Started</SubmitButton>
                     </>
-                : <>
-                    <Button block={true}>Login</Button>
-                    <Button block={true}>Register</Button>
-                </>
-                }
+                    }
 
-                <Button active={current === 'html' ? true : false} onClick={() => setCurrent('html')}>HTML</Button>
-                <Button active={current === 'css' ? true : false} onClick={() => setCurrent('css')}>CSS</Button>
-                <Button active={current === 'javascript' ? true : false} onClick={() => setCurrent('javascript')}>Javascript</Button>
-                <Editor 
-                    language={langs[current].language}
-                    displayName={langs[current].displayName}
-                    value={langs[current].value}
-                    onChange={langs[current].onChange}
-                />
+                    <Button active={current === 'html' ? true : false} onClick={() => setCurrent('html')}>HTML</Button>
+                    <Button active={current === 'css' ? true : false} onClick={() => setCurrent('css')}>CSS</Button>
+                    <Button active={current === 'javascript' ? true : false} onClick={() => setCurrent('javascript')}>Javascript</Button>
+                    <Editor 
+                        language={langs[current].language}
+                        displayName={langs[current].displayName}
+                        value={langs[current].value}
+                        onChange={langs[current].onChange}
+                    />
+                </div>
+                <div className='window display'>
+                    <iframe 
+                    srcDoc={srcDoc}
+                    title='output' 
+                    sandbox='allow-scripts'
+                    frameBorder='0'
+                    width='100%'
+                    height='100%'
+                    />
+                </div>
             </div>
-            <div className='window display'>
-                <iframe 
-                srcDoc={srcDoc}
-                title='output' 
-                sandbox='allow-scripts'
-                frameBorder='0'
-                width='100%'
-                height='100%'
-                />
-            </div>
-        </div>
-    </>
-);
+        </>
+    );
 }
 
 export default EditorPage
