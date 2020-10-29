849333d4f1a1df6b3e0c105f217b96bde6f1c73e
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-28 20:36:56 -0400

    Added page title with react helmet

--- client/src/components/GettingStarted.tsx
+++ client/src/components/GettingStarted.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Login from './Login'
 import Register from './Register'
+import Helmet from 'react-helmet'
 
 const GettingStarted = () => {
 
     return (
         <div className='container'>
+            <Helmet>
+                <title>Getting Started</title>
+            </Helmet>
             <div className='loginRegistrationContainer'>
                 <Login />
                 <Register />
