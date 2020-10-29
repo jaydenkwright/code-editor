b5894e24d948296160ca0d11fd065f884bcd82ad
Jayden Wright <jaydenkwright3@gmail.com>
2020-10-28 20:37:25 -0400

    Added page title with react helmet

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Login from './Login'
+import Helmet from 'react-helmet'
 
 const LoginPage = () => {
     return (
         <div className='container'>
+            <Helmet>
+                <title>Login</title>
+            </Helmet>
             <div className='loginRegistrationContainer'>
                 <Login />
             </div>
